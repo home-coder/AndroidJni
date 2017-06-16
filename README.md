ba6634cb507788fdc1231f3b029ab0f1c51bee25
jiangxiujie <jiangxiujie@cantv.cn>
2017-06-16 18:18:26 +0800

    参考网上Readme.md例子初步搞一个jni，没有android.mk，是必须的吗

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # android-jni
 先做一个简单的android studio中开发全新jni的过程，并且也调用第三方jni库的流程也知道。
+http://blog.csdn.net/easyer2012/article/details/52352951
--- app/src/main/java/com/example/jiangxiujie/androidjni/JniMain.java
+++ app/src/main/java/com/example/jiangxiujie/androidjni/JniMain.java
@@ -2,12 +2,26 @@ package com.example.jiangxiujie.androidjni;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class JniMain extends AppCompatActivity {
+    private static final String TAG = "JniMain";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jni_main);
+/*        Log.e(TAG, "onCreate: " + helloJni() );
+
+        Integer addResult = new Integer(addCalc(12, 13));
+        Log.e(TAG, "onCreate: " + addResult);*/
+/*        helloJni();
+        addCalc(1, 2);*/
+    }
+
+    public static native String helloJni();
+    public static native int addCalc(int a, int b);
+    static {
+        System.loadLibrary("hello_jni"); // 注意没有前缀lib和后缀.so
     }
 }
